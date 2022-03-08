ed796bc10d2120cb05fc809fbdb6debbd7c60136
Rob Brennan <rob@therobbrennan.com>
2022-03-07 16:44:00 -0800

    Revise example GraphQL unit tests and snapshots

--- apps/nextjs-with-typescript-graphql/test/index.test.tsx
+++ apps/nextjs-with-typescript-graphql/test/index.test.tsx
@@ -1,10 +1,11 @@
-import { InMemoryCache, gql } from '@apollo/client'
-import React from 'react'
-import Index from '../pages'
-import renderer from 'react-test-renderer'
-import { MockedProvider } from '@apollo/client/testing'
+import { InMemoryCache, gql } from "@apollo/client";
+import React from "react";
+import Index from "../pages";
+import renderer, { act } from "react-test-renderer";
+import { MockedProvider } from "@apollo/client/testing";
 
-const cache = new InMemoryCache()
+// Refactor cache example to a shared resource
+const cache = new InMemoryCache();
 cache.writeQuery({
   query: gql`
     query Viewer {
@@ -17,23 +18,41 @@ cache.writeQuery({
   `,
   data: {
     viewer: {
-      __typename: 'User',
-      id: 'Baa',
-      name: 'Baa',
-      status: 'Healthy',
+      __typename: "User",
+      id: "Baa",
+      name: "Baa",
+      status: "Healthy",
     },
   },
-})
+});
 
-describe('Index', () => {
-  it('renders the html we want',  () => {
-    () => {
-      const component = renderer.create(
-        <MockedProvider cache={cache}>
-          <Index />
-        </MockedProvider>
-      )
-      expect(component.toJSON()).toMatchSnapshot()
-    }
-  })
-})
+describe("Page - Index [DEFAULT]", () => {
+  it("matches the initially rendered snapshot", () => {
+    const component = renderer.create(
+      <MockedProvider cache={cache}>
+        <Index />
+      </MockedProvider>
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it("matches the fully rendered snapshot", async () => {
+    const component = renderer.create(
+      <MockedProvider cache={cache}>
+        <Index />
+      </MockedProvider>
+    );
+
+    // Fire off async events to cause the component to change here
+    await act(() => {
+      return new Promise((resolve) => {
+        // Simulate waiting for the GraphQL query response to populate our cache by waiting 10ms
+        setTimeout(() => resolve(), 10);
+      });
+    });
+
+    // Test the final state of our component
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
